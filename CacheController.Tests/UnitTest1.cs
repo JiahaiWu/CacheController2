fdaa5716dd8dd9cf22d9c72da120a741a0a3bb45
Brenton McSweyn <brenton.mcsweyn@gmail.com>
2013-01-18 04:42:09 +0000

    Initial Commit

---
+++ CacheController.Tests/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CacheController;
+
+namespace CacheController.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestInitialize]
+        public void init()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = CacheController.Cache("test", 10, new Func<DateTime>(TestFunction));
+
+            var result2 = CacheController.Cache("test", 10, new Func<DateTime>(TestFunction));
+
+            Assert.AreEqual(result, result2);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = CacheController.Cache("test", 10, new Func<DateTime>(TestFunction));
+
+            System.Threading.Thread.Sleep(15);
+
+            var result2 = CacheController.Cache("test", 10, new Func<DateTime>(TestFunction));
+
+            Assert.AreNotEqual(result, result2);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var result = CacheController.Cache("test", 10, new Func<DateTime>(TestFunction));
+
+            
+            var result2 = CacheController.Cache("test1", 10, new Func<DateTime>(TestFunction));
+
+            Assert.AreNotEqual(result, result2);
+
+        }
+
+        public DateTime TestFunction()
+        {
+
+            return DateTime.Now;
+        }
+    }
+}
---
+++ CacheController/CacheController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace CacheController
+{
+    public class CacheController
+    {
+        public static object Cache(string key, int cacheTimeMilliSeconds, Delegate result, params object[] args)
+        {
+            var cache = MemoryCache.Default;
+
+            if (cache[key] == null)
+            {
+                object cacheValue;
+                try
+                {
+                    cacheValue = result.DynamicInvoke(args);
+                }
+                catch (Exception e)
+                {
+
+                    var message = e.InnerException.GetBaseException().Message;
+                    throw new Exception(message);
+
+                }
+                if (cacheValue != null)
+                {
+                    cache.Set(key, cacheValue, DateTimeOffset.Now.AddMilliseconds(cacheTimeMilliSeconds));
+                }
+            }
+            return cache[key];
+        }
+    }
+}
